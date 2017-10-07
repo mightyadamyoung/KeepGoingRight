965540e4afc6cb8245aa553df7a916e0cd7d92f1
mightyadamyoung <mauco29@lsu.edu>
2017-10-06 23:32:23 -0500

    Initial GUI Elements added

---
+++ KeepGoingRight/Assets/UI/GUI_Label.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class GUI_Label : MonoBehaviour
+{
+    public bool debugMode = false;
+	private void OnGUI()
+    {
+        if (debugMode || Application.isPlaying)
+        {
+            GUI.color = Color.cyan;
+            GUI.Label(new Rect(200, 15, 75, 75), "Let the Game Begin");
+        }
+    }
+}
